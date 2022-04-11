86a9866d5ed64c08e51766e09c1599dc97bbeb6e
Ty Sours <tsours@ucdavis.edu>
2022-04-11 09:49:51 -0700

    Add elastic constant dict

--- strains.py
+++ strains.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 from numpy import array
 
-def voigt_to_3x3(): 
-    pass
-
 all_strain_tensors = array([
                       [1, 0, 0, 0, 0, 0], # C_11 0 
                       [0, 1, 0, 0, 0, 0], # C_22 1
@@ -30,12 +27,38 @@ all_strain_tensors = array([
 
 # NOTE: Monoclinic might need testing
 #       Triclinic could be reduced to 18
-STRAINS = {'cubic': all_strain_tensors[[0, 3, 6]],
-           'hexa': all_strain_tensors[[0, 2, 3, 6, 7]],
-           'tetra1': all_strain_tensors[[0, 2, 3, 5, 6, 7]],
-           'tetra2': all_strain_tensors[[0, 2, 3, 5, 6, 7, 10]],
-           'trigo1': all_strain_tensors[[0, 2, 3, 6, 7, 8]],
-           'trigo2': all_strain_tensors[[0, 2, 3, 6, 7, 8, 9]],
-           'ortho': all_strain_tensors[[0, 1, 2, 3, 4, 5, 6, 7, 11]],
-           'mono': all_strain_tensors[[0, 1, 2, 3, 4, 5, 6, 7, 9, 11, 13, 16, 19]],
-           'tric': all_strain_tensors}
+STRAIN_SETS = {
+    'cubic': all_strain_tensors[[0, 3, 6]],
+    'hexagonal': all_strain_tensors[[0, 2, 3, 6, 7]],
+    'tetragonal1': all_strain_tensors[[0, 2, 3, 5, 6, 7]],
+    'tetragonal2': all_strain_tensors[[0, 2, 3, 5, 6, 7, 10]],
+    'trigonal1': all_strain_tensors[[0, 2, 3, 6, 7, 8]],
+    'trigonal2': all_strain_tensors[[0, 2, 3, 6, 7, 8, 9]],
+    'orthorhombic': all_strain_tensors[[0, 1, 2, 3, 4, 5, 6, 7, 11]],
+    'monoclinic': all_strain_tensors[[0, 1, 2, 3, 4, 5, 6, 7, 9, 11, 13, 16, 19]],
+    'triclinic': all_strain_tensors
+    }
+
+STRAINS = {
+           'C_11': [1, 0, 0, 0, 0, 0], # C_11 0 
+           'C_22': [0, 1, 0, 0, 0, 0], # C_22 1
+           'C_33': [0, 0, 1, 0, 0, 0], # C_33 2
+           'C_44': [0, 0, 0, 2, 0, 0], # C_44 3
+           'C_55': [0, 0, 0, 0, 2, 0], # C_55 4
+           'C_66': [0, 0, 0, 0, 0, 2], # C_66 5
+           'C_12': [1, 1, 0, 0, 0, 0], # C_12 6
+           'C_13': [1, 0, 1, 0, 0, 0], # C_13 7
+           'C_14': [1, 0, 0, 2, 0, 0], # C_14 8
+           'C_15': [1, 0, 0, 0, 2, 0], # C_15 9
+           'C_16': [1, 0, 0, 0, 0, 2], # C_16 10
+           'C_23': [0, 1, 1, 0, 0, 0], # C_23 11
+           'C_24': [0, 1, 0, 2, 0, 0], # C_24 12
+           'C_25': [0, 1, 0, 0, 2, 0], # C_25 13
+           'C_26': [0, 1, 0, 0, 0, 2], # C_26 14
+           'C_34': [0, 0, 1, 2, 0, 0], # C_34 15
+           'C_35': [0, 0, 1, 0, 2, 0], # C_35 16
+           'C_36': [0, 0, 1, 0, 0, 2], # C_36 17
+           'C_45': [0, 0, 0, 2, 2, 0], # C_45 18
+           'C_46': [0, 0, 0, 2, 0, 2], # C_46 19
+           'C_56': [0, 0, 0, 0, 2, 2]  # C_56 20
+                   }
