5feb907c5c2558462b506ffdd786ff88eb96c07b
Ty Sours <tsours@ucdavis.edu>
2022-04-19 12:59:51 -0700

    Add descriptions

--- utils.py
+++ utils.py
@@ -4,6 +4,17 @@ from numpy import array
 
 
 def get_lattice_type(atoms, spacegroup=None, symprec=0.01):
+    """
+    finds spacegroup and corresponding crystal family (e.g., cubic)
+    of ASE Atoms object
+
+    Parameters
+    ----------
+    atoms: ASE Atoms object to find lattice type
+    spacegroup: int (optional), option to manually specify spacegroup
+    symprec: float, spglib symmetry tolerance to find spacegroup
+             higher value for less symmetric structures
+    """
     families = {
         2: "triclinic",
         15: "monoclinic",
@@ -27,6 +38,21 @@ def get_lattice_type(atoms, spacegroup=None, symprec=0.01):
 
 
 def voigt_to_full(V):
+    """
+    find full 3x3 array from 6 element vector in voigt notation
+
+                                    [[xx, xy, xz],
+    [xx, yy, zz, yz, xz, xy]  -->    [xy, yy, yz],
+                                     [xz, yz, zz]]
+
+
+
+    Parameters
+    ----------
+    V: array-like, 6 element vector in voigt notation
+    """
+
+
     xx, yy, zz, yz, xz, xy = V
     A = array(
         [[xx, 0.5 * xy, 0.5 * xz],
@@ -37,6 +63,18 @@ def voigt_to_full(V):
 
 
 def full_to_voigt(A):
+    """
+    find full (symmetric) 3x3 array from 6 element vector in voigt notation
+
+    [[xx, xy, xz],
+     [xy, yy, yz],  -->   [xx, yy, zz, yz, xz, xy]
+     [xz, yz, zz]]
+
+    Parameters
+    ----------
+    A: array-like, 3x3 array to transform into voigt notation vector
+
+    """
     A = 0.5 * (A + A.T)
     V = [A[0, 0], A[1, 1], A[2, 2], 2*A[1, 2], 2*A[0, 2], 2*A[0, 1]]
     return array(V)
