3d62100994fe12d683f3cc6089ddf1a20089f29a
Ty Sours <tsours@ucdavis.edu>
2022-12-15 11:12:44 -0800

    Add plotting capabilities

--- enstelco/main.py
+++ enstelco/main.py
@@ -1,16 +1,19 @@
-import numpy as np
-import sys
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+from ase import io
+from ase.units import GPa
 
-sys.path.insert(0, os.path.abspath('..'))
 from enstelco.deformations import Deformations
 from enstelco.solve import Solvers
-from enstelco.utils import get_lattice_type
+from enstelco.utils import get_lattice_type, colors
+
 
 # XXX: Adds dynamic inheritance based on lattice type
 #      (I should really rework solve.py to avoid this)
 def dynamic_solver(cls):
     def make_instance(atoms, **kwargs):
+        print(atoms, kwargs)
         if kwargs.get('lattice_type'):
             lattice_type = kwargs.get('lattice_type')
         else:
@@ -18,12 +21,12 @@ def dynamic_solver(cls):
 
         Solver = Solvers[lattice_type]
 
-        class NewClass(cls, Solver):
+        class RealENSTELCO(cls, Solver):
             def __init__(self):
                 cls.__init__(self, atoms, **kwargs)
                 Solver.__init__(self)
 
-        return NewClass()
+        return RealENSTELCO()
 
     return make_instance
 
@@ -35,22 +38,97 @@ class ENSTELCO(Deformations):
             self, atoms, calc=calc, lattice_type=lattice_type, verbose=verbose
         )
 
-    def read(self):
+    def read(self, opt_file='opt.traj'):
         n_sets = len(self.strain_set)
         strains = [np.loadtxt(f'{i:03d}/strains') for i in range(n_sets)]
         n_strains = len(strains[0])
-        energies = [[read(f'{i:03d}/{j:03d}/opt.traj').get_potential_energy() for j in range(n_strains)] for i in range(n_sets)]
-        ref_e = self.atoms.get_potential_energy()
-        ref_V = self.atoms.get_volume()
-        self.energies = (np.array(energies) - ref_e) / ref_V
+
+        def get_E(path):
+            return io.read(os.path.join(path, opt_file)).get_potential_energy()
+
+        energies = [[get_E(f"{i:03d}/{j:03d}") for j in range(n_strains)]
+                    for i in range(n_sets)]
+
+        self._ref_e = self.atoms.get_potential_energy()
+        self._ref_V = self.atoms.get_volume()
+        self.energies = (np.array(energies) - self._ref_e) / self._ref_V
         self.strains = np.array(strains)
 
     def process(self):
-        if not self.energies:
+        if self.energies is None:
             self.read()
         self.get_properties()
+
+    def plot(self, cij=all, n_max=4, axs=None):
+        if cij is all:
+            cij = self.order
+            if self.lattice_type in ['hexagonal', 'trigonal1', 'trigonal2']:
+                print(cij)
+                cij = [c for c in cij if c != 'C66'] # determined from symmetry
+                print(cij)
+        self._cij = cij
+
+        n_panels = int(np.ceil(len(cij) / n_max))
+        n_cols = n_panels if n_panels < 4 else 4
+        n_rows = int(np.ceil(n_panels / 4))
+        width = 0.5 + 3.5 * n_cols
+        height = 3 * n_rows
+
+        if axs is None:
+            fig, axs = plt.subplots(n_rows, n_cols, figsize=(width, height))
+        if n_cols == n_rows == 1:
+            axs = np.array([axs])
+            print(axs)
+        if axs.ndim == 1:
+            axs = [axs]
+        k = 0
+        leg_params = dict(labelcolor='mfc', frameon=0,
+                markerscale=0, handlelength=0.0, fontsize=12)
+        for a in range(n_rows):
+            for b in range(n_cols):
+                for n in range(n_max):
+                    ij = int(cij[k][-2:])
+                    self.plot_cij(ij, color=colors[n], ax=axs[a][b])
+                    k += 1
+                    if k == len(cij):
+                        break
+                axs[a][b].legend(**leg_params)
+                axs[a][b].tick_params(labelsize=12)
+                if b == 0:
+                    axs[a][b].set_ylabel('Energy', fontsize=14)
+                if a + 1 == n_rows:
+                    axs[a][b].set_xlabel('Strain (%)', fontsize=14)
+
+        plt.tight_layout()
+        plt.show()
+
+
+    def plot_cij(self, ij, color=None, ax=None):
+        if ax is None:
+            ax = plt.gca()
+
+        data, fit_data = self._get_plot_data(ij)
+        i, j = str(ij)
+        text = f'C$_{i}$$_{j}$ = {self.elastic_constants[f"C{ij}"]: .1f}'
+        ax.plot(*fit_data, ls='-', lw=2.0, color=color)
+        ax.plot(*data, ls='', marker='o', mew=1, mec='black', color=color, ms=7, label=text)
+
+    def _get_plot_data(self, ij):
+        i_read = self._cij.index(f'C{ij}')
+        A2 = self._A2[i_read]
+
+        strains = self.strains[i_read]
+        energies = self.energies[i_read] * self._ref_V + self._ref_e
+
+        fit_strains = np.linspace(min(strains), max(strains), 30)
+        fit_energies = self.get_E(A2, strains=fit_strains) * self._ref_V + self._ref_e
+
+        return (strains * 100, energies), (fit_strains * 100, fit_energies)
+
 
 if __name__ == '__main__':
     from ase.io import read
     atoms = read('opt.traj')
     enstelco = ENSTELCO(atoms)
+    enstelco.read()
+    enstelco.plot_cij(11)
