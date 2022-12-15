610deefa964bf0476a2ecb2c0f00b8322bb22c60
Ty Sours <tsours@ucdavis.edu>
2022-12-15 14:18:34 -0800

    Add deform cli command

--- enstelco/cli.py
+++ enstelco/cli.py
@@ -1,12 +1,13 @@
 import argparse
+from textwrap import dedent
 
 from enstelco import __version__, ENSTELCO
 
 commands = (
-        'deform',
-        'process',
-        'plot',
-    )
+    'deform',
+    'process',
+    'plot',
+)
 
 class Base:
     def __init__(self, parser):
@@ -19,19 +20,46 @@ class DeformCLI(Base):
     calculate energies and fit to elastic constants. Number of deformations
     is dependent on lattice symmetry (automatically determined or can be
     specified).
+
+    Examples:
+
+        $ enstelco deform CHA.cif
+        $ enstelco deform --lo -0.02 --hi 0.02 thing.xyz
+        $ enstelco deform -l cubic cubic_thing_but_improperly_detected_automatically.xyz
     """
     help_info = 'Deform input structure and write corresponding deformed inputs'
     def add_args(self):
-        pass
+        self.parser.add_argument('structure', type=str,
+            help='Undeformed structure to be used for deformations'\
+                    ' (any ASE-readable format)')
+
+        self.parser.add_argument('-n', type=int, default=5,
+                help='Number of strains to apply between --lo and --hi')
+        self.parser.add_argument('--lo', type=float, default=0.0,
+                help='Lower strain bound')
+        self.parser.add_argument('--hi', type=float, default=0.04,
+                help='Upper strain bound')
+        self.parser.add_argument('-i', '--input_file', type=str, default='POSCAR',
+                help='Name (and ext) of input file to write for each deformation')
+        self.parser.add_argument('-l', '--lattice-type', type=str, default=None,
+                help='Specific lattice type to force (automatically determined by'\
+                        ' default). Can be spacegroup number of choose from '\
+                        'enstelco.deformations.crystal_families')
 
     def main(self, args):
-        raise NotImplementedError('Coming soon...')
+        from ase.io import read
+        atoms = read(args.structure)
+        enstelco = ENSTELCO(atoms, lattice_type=args.lattice_type,
+                            input_file=args.input_file)
+        enstelco.deform(n=args.n, smin=args.lo, smax=args.hi)
 
 
 class ProcessCLI(Base):
     """
+    Calculate elastic constants and mechanical properties from completed
+    deformation calculations.
     """
-    help_info = ''
+    help_info = 'Calculate elastic constants + mechanical properties'
     def add_args(self):
         pass
 
@@ -41,8 +69,10 @@ class ProcessCLI(Base):
 
 class PlotCLI(Base):
     """
+    Plot energy-strain data from completed calculations with second order fits
+    and annotated elastic constant values.
     """
-    help_info = ''
+    help_info = 'Plot energy-strain data and fits'
     def add_args(self):
         pass
 
@@ -51,17 +81,27 @@ class PlotCLI(Base):
 
 
 cli_parsers = {
-        'deform': DeformCLI,
-        'process': ProcessCLI,
-        'plot': PlotCLI,
-    }
+    'deform': DeformCLI,
+    'process': ProcessCLI,
+    'plot': PlotCLI,
+}
+
+
+class MyFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    """
+    HelpFormatter that retains docstring formatting (mostly to preserve example
+    listing).
+    """
+
+    def _fill_text(self, text, width, indent):
+        return dedent(text)
 
 
 def main():
     parser = argparse.ArgumentParser(prog='enstelco',
                                      description='ENergy-STrain ELastic COnstants'\
                                              ' and mechanical property calculations!',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     formatter_class=MyFormatter,
                                      )
     parser.add_argument('--version', action='version', version=__version__)
     subparsers = parser.add_subparsers(title='commands', dest='command')
@@ -71,7 +111,7 @@ def main():
         CLI = cli_parsers[comm]
         doc = CLI.__doc__
         subparser = subparsers.add_parser(comm, help=CLI.help_info, description=doc,
-                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+                formatter_class=MyFormatter)
         cli = CLI(subparser)
         cli.add_args()
         command_clis[comm] = cli
