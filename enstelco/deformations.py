4f0ddeea6cf9c8f10be07d6bc07451c38217e5a1
Ty Sours <tsours@ucdavis.edu>
2022-12-20 11:58:59 -0800

    Add attribute for directory list

--- enstelco/deformations.py
+++ enstelco/deformations.py
@@ -64,6 +64,7 @@ class Deformations:
         else:
             self.strains = np.array(strains)
 
+        self._dirs = []
         for i, eta  in enumerate(self.strain_set):
             if i_rerun is not None:
                 if i != i_rerun:
@@ -75,6 +76,7 @@ class Deformations:
                 path = os.path.join(f'{i:03d}', f'{j:03d}')
                 os.makedirs(path, exist_ok=True)
                 deformed_atoms.write(os.path.join(path, self.input_file))
+                self._dirs.append(path)
 
             np.savetxt(os.path.join(f'{i:03d}', 'strains'), self.strains)
 
@@ -88,9 +90,15 @@ class Deformations:
         deformed_atoms.set_cell(new_cell, scale_atoms=True)
         return deformed_atoms
 
-    def rerun(self, i_rerun, n, smin, smax, strains=None):
+    def rerun(self, i_rerun, n, smin, smax, strains=None, deform=True):
+        """
+        Rerun specific deformation. Might be useful if certain directions give
+        poor fits and you want to manually adjust strains for that direction.
+        Maybe poor practice? :shrug:
+        """
         os.system(f'rm -r {i_rerun:03d}/0*')
-        self.deform(n=n, smin=smin, smax=smax, i_rerun=i_rerun, strains=strains)
+        if deform:
+            self.deform(n=n, smin=smin, smax=smax, i_rerun=i_rerun, strains=strains)
 
         deformation_dirs = glob.glob(f'{i_rerun:03d}/0*')
         deformation_dirs.sort()
@@ -103,15 +111,20 @@ class Deformations:
             atoms.write(self.output_file)
             os.chdir('../..')
 
-    def run(self, n=5, smin=0.0, smax=0.04, calc=None):
-        self.deform(n=n, smin=smin, smax=smax)
+    def run(self, n=5, smin=0.0, smax=0.04, calc=None, deform=True):
+        if deform:
+            self.deform(n=n, smin=smin, smax=smax)
         if calc is None:
             if self.calc is None:
                 print('NO CALCULATOR PRESENT')
                 print('ONLY PERFORMING DEFORMATIONS')
                 return
             calc = self.calc
-        deformation_dirs = sorted(glob.glob('0*/0*'))
+        if not hasattr('self', '_dirs'):
+            # user might make their own dirs for whatever reason
+            deformation_dirs = sorted(glob.glob('0*/0*'))
+        else:
+            deformation_dirs = self._dirs
 
         for d in deformation_dirs:
             os.chdir(d)
