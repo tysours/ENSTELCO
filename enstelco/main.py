5b64d2f6b79bcd24688e9b099750db3b19dc0014
Ty Sours <tsours@ucdavis.edu>
2022-12-20 11:59:50 -0800

    Add processing methods w/ fancy console output

--- enstelco/main.py
+++ enstelco/main.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ase import io
 from ase.units import GPa
+from rich.console import Console, Group
+from rich.text import Text
+from rich.panel import Panel
+from rich.table import Table
 
 from enstelco.deformations import Deformations
 from enstelco.solve import Solvers
@@ -60,6 +64,93 @@ class ENSTELCO(Deformations):
         if self.energies is None:
             self.read()
         self.get_properties()
+
+    def summarize(self, boring=False, file_name=None):
+        if not hasattr(self, 'properties'):
+            self.process()
+
+        if file_name is not None:
+            boring = True
+
+        ec_panel = self._write_ec_panel(boring=boring)
+        tensor_panel = '' if boring else self._write_tensor_panel()
+        properties_panel = self._write_properties_panel(boring=boring)
+
+        if file_name is not None:
+            with open(file_name, 'w') as file:
+                file.write(f'{ec_panel}{properties_panel}')
+            return
+
+        console = Console()
+        display = Group(ec_panel, tensor_panel, properties_panel)
+        print()
+        console.print(display)
+        print()
+
+    def _write_ec_panel(self, boring=False):
+        if boring:
+            text = ''
+            for k, v in self.elastic_constants.items():
+                text += f'{k} {v}\n'
+            return text
+
+        ec_tables = []
+        ec_vals = []
+        for i, (k, v) in enumerate(self.elastic_constants.items()):
+            if i % 7 == 0:
+                ec_tables.append(Table())
+                ec_vals.append([])
+            ec_tables[-1].add_column(k, justify='center')
+            ec_vals[-1].append(round(v, 1))
+
+        for vals, table in zip(ec_vals, ec_tables):
+            table.add_row(*list(map(str, vals)))
+
+        ec_panel = Panel(Group(*ec_tables), title='ELASTIC CONSTANTS', expand=False)
+        return ec_panel
+
+    def _write_tensor_panel(self):
+        tensor_table = Table.grid()
+        for i in range(6):
+            tensor_table.add_column(justify='center', min_width=8)
+        for j in range(6):
+            vals = np.round(self.elastic_tensor[j, :], 1)
+            tensor_table.add_row(*list(map(str, vals)))
+
+        tensor_panel = Panel(tensor_table, title='ELASTIC TENSOR', expand=False)
+        return tensor_panel
+
+    def _write_properties_panel(self, boring=False):
+        if boring:
+            text = ''
+            def append_line(text, attr):
+                text += f'{attr} {getattr(self, attr)}\n'
+                return text
+
+            attributes = [
+                'K_V', 'K_R', 'K_VRH',
+                'E_V', 'E_R', 'E_VRH',
+                'G_V', 'G_R', 'G_VRH',
+                'v_V', 'v_R', 'v_VRH',
+            ]
+
+            for a in attributes:
+                text = append_line(text, a)
+            return text
+
+        headers = ['Method', 'Bulk modulus', "Young's modulus", 'Shear modulus',
+                "Poisson's ratio"]
+        properties_table = Table(*headers)
+
+        mech_keys = ['Voigt', 'Reuss', 'Hill']
+        for key, vals in zip(mech_keys, self.properties):
+            properties_table.add_row(key, *list(map(str, np.round(vals, 2))))
+
+        title = 'MECHANICAL PROPERTIES'
+        subtitle = 'Calculated with ELATE: https://progs.coudert.name/elate'
+        properties_panel = Panel(properties_table, title=title,
+                                 subtitle=subtitle, expand=False)
+        return properties_panel
 
     def plot(self, cij=all, n_max=4, axs=None):
         if cij is all:
